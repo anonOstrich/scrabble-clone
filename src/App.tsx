90d3c07bd143fb3890b3cdabdec390479c1656af
Jesper Kuutti <jesper.kuutti@gmail.com>
2024-03-19 15:45:37 +0200

    Added tailwind and postcss to the project

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,14 @@ import './App.css'
 function App() {
 
   return (
-    <main className="m-10 text-red">
+    <main>
       <h1>Scrabble clone</h1>
-      <section>
-        <section>
-          <h2>Game board</h2>
-          <p>Game board will go here</p>
-        </section>
-        <section>
-          <h2>Player 1</h2>
-          <p>Player 1's rack will go here</p>
-        </section>
-        <section>
-          <h2>Player 2</h2>
-          <p>Player 2's rack will go here</p>
-        </section>
+      <section className='flex flex-col justify-between'>
+        <ul>
+          <li>Something</li>
+          <li>Else</li>
+          <li>And stuff</li>
+        </ul>
       </section>
     </main>
   )
