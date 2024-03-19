8e344f05b4073652c01e3bc6af2d98803f0581f4
Jesper Kuutti <jesper.kuutti@gmail.com>
2024-03-19 16:58:10 +0200

    Basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <main>
-      <h1 className="mb-4 text-[40px] underline underline-offset-[8px] decoration-wavy decoration-amber-600 decoration-[4px]">
-        Scrabble clone
-      </h1>
-      <section>
-        <ul className="space-y-4">
-          <li className="border-amber-200 border-2">Something</li>
-          <li className="border-amber-200 border-2">Else</li>
-          <li className="border-amber-200 border-2">And stuff</li>
-        </ul>
-      </section>
-    </main>
+    <>
+      <nav className="bg-amber-600">Navbar</nav>
+      <main className="bg-cyan-800 flex-1">
+        <section>Controls</section>
+        <section>Game board</section>
+      </main>
+      <footer className="bg-gray-700">Footer</footer>
+    </>
   );
 }
 
