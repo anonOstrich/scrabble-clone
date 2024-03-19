3185675dd583d44d35811564cf38989d5ffcaf42
Jesper Kuutti <jesper.kuutti@gmail.com>
2024-03-19 16:13:02 +0200

    Testing that tailwind works

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 function App() {
   return (
     <main>
-      <h1>Scrabble clone</h1>
-      <section className="flex flex-col justify-between">
-        <ul>
-          <li>Something</li>
-          <li>Else</li>
-          <li>And stuff</li>
+      <h1 className="mb-4 text-[40px] underline underline-offset-[8px] decoration-wavy decoration-amber-600 decoration-[4px]">
+        Scrabble clone
+      </h1>
+      <section>
+        <ul className="space-y-4">
+          <li className="border-amber-200 border-2">Something</li>
+          <li className="border-amber-200 border-2">Else</li>
+          <li className="border-amber-200 border-2">And stuff</li>
         </ul>
       </section>
     </main>
